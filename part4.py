1a09713adaf2542bf77e1299c431efcdca6d5b0c
Siorai Tynan <103528536+siorai-jt@users.noreply.github.com>
2022-05-24 13:46:02 +0000

    h

--- part4.py
+++ part4.py
@@ -12,4 +12,4 @@ Define a function possibletriangle that takes 3 arguments (side1, side2, side3).
 '''
 
 def possibletriangle(side1, side2, side3): #do not change this line
-  pass #delete the word pass when you start writing your code
+  ¿¿¿¿¿¿¿¿
